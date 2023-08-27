232c5e4e6f39b5c97fc3881d234656995d428e12
Lucas Silva <gaviao.lucas@gmail.com>
2023-08-27 15:53:47 +0100

    styling some pages

--- src/pages/Contact/ContactPage.js
+++ src/pages/Contact/ContactPage.js
@@ -7,17 +7,28 @@ import style from "./contact.module.css";
 function ContactPage() {
   const [spinner, setSpinner] = useState(false);
   const [body, setBody] = useState("");
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
   const navigate = useNavigate();
 
-  const handleChange = (e) => {
+  const handleChangeBody = (e) => {
     setBody(e.target.value);
-    console.log(body);
   };
+
+  const handleChangeSubject = (e) => {
+    setSubject(e.target.value);
+    console.log(subject);
+  };
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+    console.log(name);
+  };
+
   const handleSend = () => {
     setSpinner(!spinner);
     setTimeout(() => {
       window.open(
-        `mailto:gaviao.lucas@gmail.com?subject=myWebsite&body=${body}`
+        `mailto:gaviao.lucas@gmail.com?subject=${name} - ${subject}&body=${body}`
       );
       navigate("/");
     }, 1000);
@@ -33,31 +44,53 @@ function ContactPage() {
         {" "}
         <span>
           I am looking for a Full-Time position, but I am also interested in
-          freelancing and collabing with other devs. <br />
-          Let's create something amazing together!
+          freelancing and collabing with other devs. Let's create something
+          amazing together!
         </span>
       </section>
+      <div className={style.sub_title}>
+        <span>
+          <img src="" alt="" />
+          +353 83 3795009
+        </span>
+        <span>
+          <img src="" alt="" />
+          gaviao.lucas@gmail.com
+        </span>
+        <span>
+          <img src="" alt="" />
+          Dublin, Ireland
+        </span>
+      </div>
 
       <Form className={style.Form}>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-2">
           <Form.Control
             className={style.input}
             type="text"
             placeholder="Name"
+            value={name}
+            onChange={(e) => {
+              handleChangeName(e);
+            }}
           />
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-2">
           <Form.Control
             className={style.input}
             type="email"
             placeholder="Email"
           />
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-2">
           <Form.Control
             className={style.input}
             type="text"
             placeholder="Subject"
+            value={subject}
+            onChange={(e) => {
+              handleChangeSubject(e);
+            }}
           />
         </Form.Group>
         <Form.Group className="mb-3">
@@ -68,7 +101,7 @@ function ContactPage() {
             value={body}
             placeholder="Message"
             onChange={(e) => {
-              handleChange(e);
+              handleChangeBody(e);
             }}
           />
         </Form.Group>
