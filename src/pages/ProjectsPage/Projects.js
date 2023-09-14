a27bb91c5fa220972a2cd8db5ad80b5fffe3dcdb
Lucas Silva <gaviao.lucas@gmail.com>
2023-09-14 22:27:13 +0100

    nearly done, final touches

--- src/pages/ProjectsPage/Projects.js
+++ src/pages/ProjectsPage/Projects.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import classes from "./projects.module.css";
 import { BsGithub } from "react-icons/bs";
 import project1 from "../../images/projects/Screenshot 2023-09-13 at 21.15.03.png";
@@ -56,142 +58,162 @@ function Projects() {
     <section className={classes.ProjectsPage}>
       <h1>PROJECTS</h1>
 
-      <p></p>
       <CardGroup className={classes.myCardGroup}>
-        <Card
-          className={classes.card}
-          bg="dark"
-          style={{ width: "18rem", color: "rgba(186, 186, 186, 0.713)" }}
+        <Row
+          xs={1}
+          md={2}
+          lg={3}
+          className="g-4"
+          style={{
+            width: "100vw",
+            display: "flex",
+            justifyContent: "flex-start",
+          }}
         >
-          <Card.Img
-            className={classes.imgs}
-            variant="top"
-            bg="dark"
-            src={project1}
-          />
-          <Card.Body>
-            <Card.Title className={classes.headcard}>Star Lord</Card.Title>
-            <Card.Text>
-              Star-Wars themed game, built using DOM, OOP, HTML, CSS and vanilla
-              JavaScrpit.
-            </Card.Text>
-            {spinner1 ? (
-              <SpinnerComp />
-            ) : (
-              <>
-                <Button
-                  variant="secondary"
-                  onClick={(e) => clickDemoProjectOne()}
-                >
-                  View Demo
-                </Button>
-                <Button
-                  className={classes.gitHubBtn}
-                  variant="light"
-                  onClick={(e) => {
-                    clickGitHubProjectOne();
-                  }}
-                >
-                  <BsGithub
-                    className={classes.githublogo}
-                    size="22px"
-                    text="black"
-                    opacity="100%"
-                  />{" "}
-                  Github
-                </Button>
-              </>
-            )}
-          </Card.Body>
-        </Card>
-        <Card
-          className={classes.card}
-          bg="dark"
-          text="rgba(186, 186, 186, 0.513)"
-          style={{ width: "18rem", color: "rgba(186, 186, 186, 0.713)" }}
-        >
-          <Card.Img variant="top" bg="dark" src={project2} />
-          <Card.Body>
-            <Card.Title className={classes.headcard}>Budbudgeting</Card.Title>
-            <Card.Text>
-              Comprehensive financial management platform created using MERN
-              Stack and Bootstrap for styling.
-            </Card.Text>
-            {spinner2 ? (
-              <SpinnerComp />
-            ) : (
-              <>
-                <Button
-                  variant="secondary"
-                  onClick={(e) => {
-                    clickDemoProjectTwo();
-                  }}
-                >
-                  View Demo
-                </Button>
-                <Button
-                  className={classes.gitHubBtn}
-                  variant="light"
-                  onClick={(e) => {
-                    clickGitHubProjectTwo();
-                  }}
-                >
-                  <BsGithub
-                    className={classes.githublogo}
-                    size="24px"
-                    color="black"
-                    opacity="100%"
-                  />{" "}
-                  Github
-                </Button>
-              </>
-            )}
-          </Card.Body>
-        </Card>
-        <Card
-          className={classes.card}
-          bg="dark"
-          text="white"
-          style={{ width: "18rem", color: "rgba(186, 186, 186, 0.713)" }}
-        >
-          <Card.Img variant="top" bg="dark" src={project3} />
-          <Card.Body>
-            <Card.Title className={classes.headcard}>Little Moments</Card.Title>
-            <Card.Text>
-              Gift List Website leveraging its own API created with and MERN
-              Stack, bootstrap for styling.
-            </Card.Text>
-            {spinner3 ? (
-              <SpinnerComp />
-            ) : (
-              <>
-                <Button
-                  variant="secondary"
-                  onClick={(e) => {
-                    clickDemoProjectThree();
-                  }}
-                >
-                  View Demo
-                </Button>
-                <Button
-                  className={classes.gitHubBtn}
-                  variant="light"
-                  onClick={(e) => {
-                    clickGitHubProjectThree();
-                  }}
-                >
-                  <BsGithub
-                    className={classes.githublogo}
-                    size="24px"
-                    color="black"
-                    opacity="100%"
-                  />{" "}
-                  Github
-                </Button>
-              </>
-            )}
-          </Card.Body>
-        </Card>
+          <Col>
+            <Card
+              className={classes.card}
+              bg="dark"
+              style={{ color: "rgba(186, 186, 186, 0.713)" }}
+            >
+              <Card.Img
+                className={classes.imgs}
+                variant="top"
+                bg="dark"
+                src={project1}
+              />
+              <Card.Body>
+                <Card.Title className={classes.headcard}>Star Lord</Card.Title>
+                <Card.Text>
+                  Star-Wars themed game which was created using DOM, OOP, HTML,
+                  CSS and vanilla JavaScrpit.
+                </Card.Text>
+                {spinner1 ? (
+                  <SpinnerComp />
+                ) : (
+                  <>
+                    <Button
+                      variant="secondary"
+                      onClick={(e) => clickDemoProjectOne()}
+                    >
+                      View Demo
+                    </Button>
+                    <Button
+                      className={classes.gitHubBtn}
+                      variant="light"
+                      onClick={(e) => {
+                        clickGitHubProjectOne();
+                      }}
+                    >
+                      <BsGithub
+                        className={classes.githublogo}
+                        size="22px"
+                        text="black"
+                        opacity="100%"
+                      />{" "}
+                      Github
+                    </Button>
+                  </>
+                )}
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card
+              className={classes.card}
+              bg="dark"
+              text="rgba(186, 186, 186, 0.513)"
+              style={{ color: "rgba(186, 186, 186, 0.713)" }}
+            >
+              <Card.Img variant="top" bg="dark" src={project2} />
+              <Card.Body>
+                <Card.Title className={classes.headcard}>
+                  Budbudgeting
+                </Card.Title>
+                <Card.Text>
+                  Comprehensive financial management platform created using MERN
+                  Stack and Bootstrap for styling.
+                </Card.Text>
+                {spinner2 ? (
+                  <SpinnerComp />
+                ) : (
+                  <>
+                    <Button
+                      variant="secondary"
+                      onClick={(e) => {
+                        clickDemoProjectTwo();
+                      }}
+                    >
+                      View Demo
+                    </Button>
+                    <Button
+                      className={classes.gitHubBtn}
+                      variant="light"
+                      onClick={(e) => {
+                        clickGitHubProjectTwo();
+                      }}
+                    >
+                      <BsGithub
+                        className={classes.githublogo}
+                        size="24px"
+                        color="black"
+                        opacity="100%"
+                      />{" "}
+                      Github
+                    </Button>
+                  </>
+                )}
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card
+              className={classes.card}
+              bg="dark"
+              style={{ color: "rgba(186, 186, 186, 0.713)" }}
+            >
+              <Card.Img variant="top" bg="dark" src={project3} />
+              <Card.Body>
+                <Card.Title className={classes.headcard}>
+                  Little Moments
+                </Card.Title>
+                <Card.Text>
+                  Gift List Website leveraging its own API created with and MERN
+                  Stack, bootstrap for styling.
+                </Card.Text>
+                {spinner3 ? (
+                  <SpinnerComp />
+                ) : (
+                  <>
+                    <Button
+                      variant="secondary"
+                      onClick={(e) => {
+                        clickDemoProjectThree();
+                      }}
+                    >
+                      View Demo
+                    </Button>
+                    <Button
+                      className={classes.gitHubBtn}
+                      variant="light"
+                      onClick={(e) => {
+                        clickGitHubProjectThree();
+                      }}
+                    >
+                      <BsGithub
+                        className={classes.githublogo}
+                        size="24px"
+                        color="black"
+                        opacity="100%"
+                      />{" "}
+                      Github
+                    </Button>
+                  </>
+                )}
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </CardGroup>
     </section>
   );
